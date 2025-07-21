0b1d1a3a7ded5241d11b1a5c73afab13ea6dbd45
sabina <sabina.yeasmin@doer.com.bd>
2025-07-21 22:18:39 +0600

    lacating element

--- Selenium_Basics/04_locating_elements.py
+++ Selenium_Basics/04_locating_elements.py
@@ -1,18 +1,33 @@
 import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
 
+driver = webdriver.Chrome()
 driver.maximize_window()
 
-
 driver.get("https://muntasir101.github.io/Modern-Bank-Portal/")
+time.sleep(3)
 
-customer_registration_button = driver.find_element(By.CSS_SELECTOR,"homeRegisterCustomerBtnMain")
+customer_registration_button = driver.find_element(By.CSS_SELECTOR,"#homeRegisterCustomerBtnMain")
 
 customer_registration_button.click()
 
-time.sleep(2)
-
-full_name = driver.find_element(By.CSS_SELECTOR,"#customerNameReg")
+full_name = driver.find_element(By.CSS_SELECTOR, "#customerNameReg")
 full_name.send_keys("Mr.Alice")
+
+email = driver.find_element(By.CSS_SELECTOR, "#customerEmailReg")
+email.send_keys("alice@gmail.com")
+
+password = driver.find_element(By.CSS_SELECTOR, "#customerPasswordReg")
+password.send_keys("123456")
+
+initial_deposit = driver.find_element(By.CSS_SELECTOR, "#initialDepositReg")
+initial_deposit.send_keys("1000")
+
+register_button = driver.find_element(By.CSS_SELECTOR, "form[id='customerRegisterForm'] button[type='submit']")
+register_button.click()
+
+time.sleep(5)
+
+
