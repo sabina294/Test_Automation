8084e058339669d6a7a11efdef9c94f63e312240
sabina <sabina.yeasmin@doer.com.bd>
2025-07-22 18:57:14 +0600

    customer login, manager login done

--- 01-manager-role/01_manager_registration_login_test.py
+++ 01-manager-role/01_manager_registration_login_test.py
@@ -34,3 +34,20 @@ back_home = driver.find_element(By.CSS_SELECTOR, "form[id='managerRegisterForm']
 back_home.click()
 
 time.sleep(4)
+
+manager_login = driver.find_element(By.CSS_SELECTOR,"#homeLoginManagerBtn")
+manager_login.click()
+
+email = driver.find_element(By.CSS_SELECTOR, "#loginEmail")
+email.send_keys("test@example.com")
+time.sleep(3)
+
+password = driver.find_element(By.CSS_SELECTOR, "#loginPassword")
+password.send_keys("123456")
+time.sleep(3)
+
+manager_login = driver.find_element(By.CSS_SELECTOR, "form[id='loginForm'] button[type='submit']")
+manager_login.click()
+
+time.sleep(4)
+
